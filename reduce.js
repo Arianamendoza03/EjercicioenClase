e0d087af4fe5a62baee32c2d8e35ea1fc05541d9
ArianaMendonza03 <mendozaariana954@gmail.com>
2022-01-31 21:13:13 +1200

    ejercicio en clase

--- reduce.js
+++ reduce.js
@@ -8,3 +8,59 @@ var total = integrado.reduce(function(a, b){
     return a * b;
     });
 console.log ("La multiplicacion de la matriz es:", total);
+
+//se tiene un vector cpn los siguientes elementos 
+//imput=[30,48,5,32]
+//se pide diseñar la funcion que devuelva la siguiente cadena;
+//"tus numeros de la suerte son: 30, 48,11,5 y 32"
+//use la funcion reduce 
+const arrayNumeros=[30,48,11,5, 32];
+function numerosDelaSuerte(array){
+    const cad="Tus numeros de la suerte son:";
+    return cad + array.reduce((ac, va, index, array) =>{ 
+        var aux = index  <(array.length-1) ? "," : "y";
+        return(ac)+ aux + (va);
+    });
+}
+
+    console.log (numerosDelaSuerte(arrayNumeros));
+    //El metodo some() comprueba si al menos un elemnto del array
+    //cumple con la condicion implementada por la funcion proporcioada 
+
+    //verificar si uno de los elementos del array es mayor a 10
+     const goodInput =[8,9,10,11,16];
+     function numeroMayorq10(array){
+         return array.some(data => {
+             return data > 10;
+         })
+     }
+     console.log(numeroMayorq10(goodInput));
+//1)Verificar si una de las cadenas de caracteres del array 
+//tiene mas de 10 caracteres 
+const vectorCadenas=["Ecuador","Argentina","Dinamarca","Corea del Norte"];
+function cadenadecaracteresMayorq10(array){
+    return array.some(data => {
+        return data > 10;
+    })
+}
+console.log(cadenadecaracteresMayorq10(vectorCadenas));
+
+
+
+
+//2)chequear si uno de los elementos de la matriz es true 
+   const vectorBooleano = [ 
+   [false,false,false]
+    [false,false,false]
+    [false, false, true]
+]
+ 
+//3)verificar si la palabra'lost' existe dentro del siguiente array:
+    var vectorParrafo = ["Strange is the night where black stars rise,","Lost", "And strange moons circle through the skies,",
+  "But stranger Lost still is","Lost Carcosa."
+  ];
+if(vectorParrafo.includes("Lost")){
+  console.log("La palabra Lost esta presente");
+}else{
+  console.log("La palabra Lost no encontrada");
+}
